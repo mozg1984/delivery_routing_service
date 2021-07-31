3527a7583b8ed011a0b6921bd4be138765327b21
Radik Khisamutdinov <mozg1984@gmail.com>
2021-08-01 00:32:02 +0400

    Added definitions

---
+++ pkg/api/v1/definition.go
@@ -0,0 +1,22 @@
+package v1
+
+type Coordinates struct {
+	Latitude  float32 `json:"lan"`
+	Longitude float32 `json:"lng"`
+}
+
+type DeliveryID uint
+
+type Delivery struct {
+	ID           DeliveryID  `json:"id"`
+	Pickup       Coordinates `json:"pickup"`
+	Dropoff      Coordinates `json:"dropoff"`
+	CreationTime string      `json:"creation_time"`
+}
+
+type NewDeliveryRequest struct {
+	ID           DeliveryID  `json:"id"`
+	Pickup       Coordinates `json:"pickup"`
+	Dropoff      Coordinates `json:"dropoff"`
+	CreationTime string      `json:"creation_time"`
+}
