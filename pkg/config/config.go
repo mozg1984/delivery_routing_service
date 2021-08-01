662d4f3bde634737daf5e2f8cbdbe450795d204d
Radik Khisamutdinov <mozg1984@gmail.com>
2021-08-01 18:50:29 +0400

    Added configurations

---
+++ pkg/config/config.go
@@ -0,0 +1,49 @@
+package config
+
+import (
+	"github.com/spf13/viper"
+)
+
+type Server struct {
+	Host string
+	Port string
+}
+
+type Redis struct {
+	Auth        string
+	DB          int
+	Host        string
+	Port        string
+	MaxIdle     int
+	MaxActive   int
+	IdleTimeout int
+}
+
+type Config struct {
+	Server Server
+	Redis  Redis
+}
+
+func NewConfig() (*Config, error) {
+	config = Config{
+		Server: Server{},
+		Redis:  Redis{},
+	}
+
+	viper.SetConfigName("config")
+	viper.AddConfigPath(".")
+	viper.SetConfigType("yml")
+	viper.AutomaticEnv()
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	err = viper.Unmarshal(&config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
