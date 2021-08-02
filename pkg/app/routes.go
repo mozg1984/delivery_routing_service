0b49ec2ea7edfc7fc63996652636d89651e4f3ca
Radik Khisamutdinov <mozg1984@gmail.com>
2021-08-02 20:51:20 +0400

    Server refactoring

--- pkg/app/routes.go
+++ pkg/app/routes.go
@@ -2,14 +2,16 @@ package app
 
 import "github.com/julienschmidt/httprouter"
 
-func (s *Server) Routes() *httprouter.Router {
+type Router *httprouter.Router
+
+func NewRouter(handler *Handler) *httprouter.Router {
 	router := httprouter.New()
 
 	{
-		router.POST("/deliveries/add_delivery", s.handler.CreateDelivery)
-		router.GET("/deliveries/list", s.handler.GetDeliveries)
-		router.GET("/deliveries/", s.handler.GetDelivery)
-		router.GET("/deliveries/route_distance", s.handler.GetRouteDistance)
+		router.POST("/deliveries/add_delivery", handler.CreateDelivery)
+		router.GET("/deliveries/list", handler.GetDeliveries)
+		router.GET("/deliveries/", handler.GetDelivery)
+		router.GET("/deliveries/route_distance", handler.GetRouteDistance)
 	}
 
 	return router
--- pkg/app/server.go
+++ pkg/app/server.go
@@ -1,24 +1,36 @@
 package app
 
 import (
+	"log"
+	"net/http"
+
 	"github.com/julienschmidt/httprouter"
 	"github.com/mozg1984/delivery_routing_service/pkg/api"
+	"github.com/mozg1984/delivery_routing_service/pkg/config"
 )
 
 type Server struct {
 	router          *httprouter.Router
-	handler         *Handler
 	deliveryService api.DeliveryService
 }
 
-func NewServer(router *httprouter.Router, handler *Handler, deliveryService api.DeliveryService) *Server {
+func NewServer(router *httprouter.Router, deliveryService api.DeliveryService) *Server {
 	return &Server{
 		router:          router,
-		handler:         handler,
 		deliveryService: deliveryService,
 	}
 }
 
-func (s *Server) Run() error {
+func (s *Server) Run(config *config.Server) error {
+	var addr string = config.Host + ":" + config.Port
+
+	log.Print("Starting server")
+
+	err := http.ListenAndServe(addr, s.router)
+	if err != nil {
+		log.Printf("An error occurred while starting the server: %v", err)
+		return err
+	}
+
 	return nil
 }
